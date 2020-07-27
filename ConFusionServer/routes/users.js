6d2661420daff99d616c13f53a36f60d7dab08ae
akshaya222 <akshayareddy22111>
2020-07-27 17:05:50 +0530

    Cross-Origin Resource Sharing(CORS)

--- ConFusionServer/routes/users.js
+++ ConFusionServer/routes/users.js
@@ -4,13 +4,13 @@ const bodyParser = require('body-parser');
 var passport = require('passport');
 var User = require('../models/user');
 var authenticate = require('../authenticate');
-const user = require('../models/user');
+const cors = require('./cors');
 
 
 
 router.use(bodyParser.json());
 /* GET users listing. */
-router.get('/',authenticate.verifyUser,authenticate.verifyAdmin, function(req, res, next) {
+router.get('/', cors.corsWithOptions,authenticate.verifyUser,authenticate.verifyAdmin, function(req, res, next) {
   User.find({})
   .then((users) => {
     res.statusCode = 200;
@@ -21,7 +21,7 @@ router.get('/',authenticate.verifyUser,authenticate.verifyAdmin, function(req, r
 });
 
 //code for signup
-router.post('/signup', (req, res, next) => {
+router.post('/signup',cors.corsWithOptions,  (req, res, next) => {
   User.register(new User({username: req.body.username}), 
     req.body.password, (err, user) => {
     if(err) {
@@ -51,7 +51,7 @@ router.post('/signup', (req, res, next) => {
   });
 });
 //code for login
-router.post('/login', passport.authenticate('local'), (req, res) => {
+router.post('/login',cors.corsWithOptions,  passport.authenticate('local'), (req, res) => {
 
   var token = authenticate.getToken({_id: req.user._id});
   res.statusCode = 200;
@@ -60,7 +60,7 @@ router.post('/login', passport.authenticate('local'), (req, res) => {
 });
 
 //code for logout
-router.get('/logout', (req, res) => {
+router.get('/logout', cors.corsWithOptions,(req, res) => {
 if (req.session) {
   req.session.destroy();
   res.clearCookie('session-id');
